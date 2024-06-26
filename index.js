bbf9c40a8df8fa17a7f0fcefc746c9359d0417cd
Megan McNeill <mcneim2@gmail.com>
2024-06-25 21:31:05 -0400

    moved inputs to inputs.js and exported, required inputs in index, created shapes.js

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 // Include inquirer package
 const inquirer = require("inquirer");
 // Include fs
-const fs = require("fs")
+const fs = require("fs");
+//include inputs
+const inputs = require("./inputs");
 
-const inputs = [
-    {
-        type: 'input',
-        name: 'letters',
-        message: 'Enter up to 3 letters for your logo'
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'What color should your text be?'
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape for your logo',
-        choices: ['square', 'circle', 'triangle']
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'What color should your shape be?'
-    }
-]
+
 
 // initialize the app
 function init() {
@@ -34,7 +14,9 @@ function init() {
         .prompt(inputs)
         .then((answers) => {
             console.log(answers)
-        })
+        });
+    //write the .svg file
+
 }
 
 init();
---
+++ inputs.js
@@ -0,0 +1,25 @@
+const inputs = [
+    {
+        type: 'input',
+        name: 'letters',
+        message: 'Enter up to 3 letters for your logo'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color should your text be?'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for your logo',
+        choices: ['square', 'circle', 'triangle']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color should your shape be?'
+    }
+]
+
+module.exports = inputs;
