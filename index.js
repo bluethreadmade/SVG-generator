df5a100e543f1441f08aac3fad236f4ab449a3af
Megan McNeill <mcneim2@gmail.com>
2024-06-25 21:09:43 -0400

    created prompt questions

--- index.js
+++ index.js
@@ -2,3 +2,27 @@
 const inquirer = require("inquirer");
 // Include fs
 const fs = require("fs")
+
+const inputs = [
+    {
+        type: 'input',
+        name: 'letters',
+        message: 'Enter up to 3 letters for your logo'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color should your text be?'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for your logo',
+        choices: ['square', 'circle', 'triangle']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color should your shape be?'
+    }
+]
