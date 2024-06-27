3ff4040e67cdd59f7342cd9d52cf4ddfdf641b1b
Megan McNeill <mcneim2@gmail.com>
2024-06-27 15:21:22 -0400

    refactored shapes.js to use superclass Shape and inherit to shape specific classes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 //include inputs
 const inputs = require("./inputs");
 // include Text class and Shape class
-const { Text, Shape } = require("./lib/shapes");
+const { Shape, Triangle } = require("./lib/shapes");
 
 // initialize the app
 function init() {
@@ -15,19 +15,17 @@ function init() {
         .then((answers) => {
             console.log(answers);
             //get SVG file content
-            const mySVGText = new Text(answers.letters, answers.textColor);
-            const mySVGShape = new Shape(answers.shape, answers.shapeColor)
+            const mySVG = new Triangle(answers.letters, answers.textColor, answers.shapeColor);
 
-            function generateSVG(mySVGText, mySVGShape) {
+            function renderSVG(mySVG) {
                 return `<svg width="600" height="300" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                    ${mySVGShape.generateSVGShape()}
-                    ${mySVGText.generateSVGText()}
+                    ${mySVG.renderTextAndShape()}
                 </svg>`;
                 
             };
 
             // write the SVG file
-            fs.writeFile('logo.svg', generateSVG(mySVGText, mySVGShape), (err) => {
+            fs.writeFile('logo.svg', renderSVG(mySVG), (err) => {
                 if (err) {
                     console.error(err);
                 } else {
